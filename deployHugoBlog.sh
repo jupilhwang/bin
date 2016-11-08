6b4f01afb918777152e429b57ea9634c3a3eb9dd
Jupil Hwang <jupil.hwang@gmail.com>
2016-11-08 14:09:53 +0900

    hugo deploy

---
+++ deployHugoBlog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+cd /home/jhwang/.jhwangSites
+hugo
+cd public
+git add -A
+msg="rebuilding site `date`"
+if [ $# -eq -1]
+	then msg="$1"
+fi
+
+git commit -m "$msg"
+
+git push origin master
+cd ..
+
